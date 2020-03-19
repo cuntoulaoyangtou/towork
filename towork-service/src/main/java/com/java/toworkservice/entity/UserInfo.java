7659d5f9a9963a12bbb3d541db345ea25d0c1d9d
ClassOne-t1 <jintian0xia6@163.com>
2020-03-19 13:43:33 +0800

    Default Changelist

--- towork-service/src/main/java/com/java/toworkservice/entity/UserInfo.java
+++ towork-service/src/main/java/com/java/toworkservice/entity/UserInfo.java
@@ -17,7 +17,12 @@ public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer uid;
-    private String username;
+    private String username;//姓名
+    private Integer age;//年龄
+    private Integer sex;//性别 1男 2女
+    private String address;//家庭住址
+    private String department;//所在车间
+    private String grade;//所在班次
     private String password;
 
     public Integer getUid() {
@@ -34,6 +39,46 @@ public class UserInfo {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
     }
 
     public String getPassword() {
