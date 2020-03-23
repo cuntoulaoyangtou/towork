24407447c53bb2de3e09bb9bf78e94cf4fc1916d
村头老杨头 <2962598997@qq.com>
2020-03-23 16:04:27 +0800

    跨域配置

--- vue-element-admin-towork/src/utils/request.js
+++ vue-element-admin-towork/src/utils/request.js
@@ -6,7 +6,7 @@ import Qs from 'qs'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://localhost:8081', // url = base url + request url
+  baseURL: 'http://192.168.31.77', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
@@ -15,6 +15,8 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
+
+    config.headers['Access-Control-Allow-Origin'] = '*'
     if (config.method === 'post') {
       config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
       config.headers.Accept = '*/*'
@@ -52,7 +54,7 @@ service.interceptors.response.use(
     if (res.code != null && res.code === 'SUCCESS') {
       return res
     }
-    if (res.code != null && res.code === 'NOT_TOKEN'){
+    if (res.code != null && res.code === 'NOT_TOKEN') {
       MessageBox.confirm('您已经登出，您可以取消以停留在此页面，或再次登录', '确认退出', {
         confirmButtonText: '去登陆',
         cancelButtonText: '关闭',
@@ -73,7 +75,7 @@ service.interceptors.response.use(
   error => {
     console.log('err' + error) // for debug
     Message({
-      message: error.message=='Network Error'?'网络错误':error.message,
+      message: error.message === 'Network Error' ? '网络错误' : error.message,
       type: 'error',
       duration: 5 * 1000
     })
